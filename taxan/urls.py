36348684e5d9f9e501bc99edfb783c89d7849a5d
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2025-09-23 06:53:49 +0200

    Remove "api" prefix from OpenAPI docs

--- taxan/urls.py
+++ taxan/urls.py
@@ -31,8 +31,8 @@ router.register(r'attachments', AttachmentViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('', include(router.urls)),
 ]
 
